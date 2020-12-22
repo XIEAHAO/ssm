60d19f9440443cc9870d45a234b0ac0c2aeb822b
smallyu1102 <1903941668@qq.com>
2020-12-22 18:25:49 +0800

    hhhh

---
+++ src/main/java/com/zking/ssm/ly/controller/aa.java
@@ -0,0 +1,12 @@
+package com.zking.ssm.ly.controller;
+
+/**
+ * @author smallyu
+ * @create  2020-12-22 18:24
+ */
+public class aa {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
